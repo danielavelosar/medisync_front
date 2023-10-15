eebf6fa875f5204a385e0b7d43b98126cf8e3a3d
unknown <paula.velosa2704@gmail.com>
2023-10-15 01:48:20 -0500

    [styles] login page

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+          colorScheme: ColorScheme(
+              primary: const Color.fromARGB(255, 10, 166, 150),
+              secondary: Color.fromARGB(255, 23, 103, 95),
+              surface: const Color.fromARGB(255, 10, 166, 150),
+              background: Color.fromARGB(255, 239, 243, 243),
+              error: Color.fromARGB(255, 255, 0, 0),
+              onPrimary: Color.fromARGB(255, 65, 211, 196),
+              onSecondary: Color.fromARGB(255, 14, 87, 79),
+              onSurface: Color.fromARGB(255, 127, 176, 171),
+              onBackground: const Color.fromARGB(255, 10, 166, 150),
+              onError: Color.fromARGB(255, 34, 110, 103),
+              brightness: Brightness.light),
           useMaterial3: true,
+          textTheme: TextTheme(
+              titleMedium: TextStyle(
+                  fontFamily: GoogleFonts.karla().fontFamily,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white),
+              labelSmall: TextStyle(
+                  fontFamily: GoogleFonts.karla().fontFamily,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Color.fromARGB(255, 10, 166, 150))),
           fontFamily: GoogleFonts.karla().fontFamily,
         ),
         home: const LoginPage());
--- lib/presentation/screens/login_page.dart
+++ lib/presentation/screens/login_page.dart
@@ -47,60 +47,132 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Login'),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              if (_error != "")
-                Text(
-                  _error,
-                  style: const TextStyle(color: Colors.red),
-                ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  controller: _cardIdController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'CardId',
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 250,
+            width: double.maxFinite,
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    )),
+                child: Padding(
+                  padding:
+                      //el padding de 20 a 50 cuando la pantalla es mas grande
+                      MediaQuery.of(context).size.width > 400
+                          ? const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 20)
+                          : const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 50),
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(50),
+                          bottomRight: Radius.circular(50),
+                        ),
+                        image: const DecorationImage(
+                          image: AssetImage(
+                              'lib/assets/images/logo_blanco_medisync.png'),
+                          fit: BoxFit.contain,
+                        )),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: ElevatedButton(
-                  onPressed: () {
-                    _loginButtonAction(
-                        _cardIdController.text, _passwordController.text);
-                  },
-                  child: const Text('Login'),
-                ),
-              ),
-            ],
+                )),
           ),
-        ),
+          Expanded(
+            flex: 7,
+            child: SafeArea(
+              top: false,
+              right: false,
+              left: false,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60),
+                    )),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        if (_error != "")
+                          Text(
+                            _error,
+                            style: const TextStyle(color: Colors.red),
+                          ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: TextField(
+                            controller: _cardIdController,
+                            decoration: InputDecoration(
+                              labelStyle:
+                                  Theme.of(context).textTheme.labelSmall,
+                              border: UnderlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    width: 2,
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  )),
+                              labelText: 'CardId',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: TextField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelStyle:
+                                  Theme.of(context).textTheme.labelSmall,
+                              border: UnderlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .primary)),
+                              labelText: 'Password',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              _loginButtonAction(_cardIdController.text,
+                                  _passwordController.text);
+                            },
+                            style: ElevatedButton.styleFrom(
+                                elevation: 5, shadowColor: Colors.black),
+                            child: Text('Login',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white)),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
